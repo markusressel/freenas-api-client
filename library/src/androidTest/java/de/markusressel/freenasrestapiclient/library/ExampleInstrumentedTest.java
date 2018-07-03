5050af923a087c855e729e483f1a0c829a749b55
Markus Ressel <markus9100@gmail.com>
2018-07-03 17:57:45 +0200

    added codecov to travis & gradle fixed example junit test

--- library/src/androidTest/java/de/markusressel/freenasrestapiclient/library/ExampleInstrumentedTest.java
+++ library/src/androidTest/java/de/markusressel/freenasrestapiclient/library/ExampleInstrumentedTest.java
@@ -38,6 +38,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("de.markusressel.library.test", appContext.getPackageName());
+        assertEquals("de.markusressel.freenasrestapiclient.library.test", appContext.getPackageName());
     }
 }
