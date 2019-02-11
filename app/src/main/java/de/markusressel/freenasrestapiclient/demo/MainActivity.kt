e5b997fff58762909fba13a2afe71eb526c44de6
Markus Ressel <mail@markusressel.de>
2019-02-11 01:34:02 +0100

    import fixes

--- app/src/main/java/de/markusressel/freenasrestapiclient/demo/MainActivity.kt
+++ app/src/main/java/de/markusressel/freenasrestapiclient/demo/MainActivity.kt
@@ -21,10 +21,12 @@ package de.markusressel.freenasrestapiclient.demo
 import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Lifecycle
+import com.trello.rxlifecycle2.android.lifecycle.kotlin.bindUntilEvent
 import de.markusressel.commons.android.material.toast
 import de.markusressel.freenasrestapiclient.api.v1.BasicAuthConfig
 import de.markusressel.freenasrestapiclient.api.v1.FreeNasWebApiClient
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.rxkotlin.subscribeBy
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
