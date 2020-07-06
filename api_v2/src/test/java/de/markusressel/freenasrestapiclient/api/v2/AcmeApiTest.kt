f173b2c8f5d34b1eeb9cde1be56cfc3310b14234
Markus Ressel <mail@markusressel.de>
2020-07-06 02:55:47 +0200

    added test

---
+++ api_v2/src/test/java/de/markusressel/freenasrestapiclient/api/v2/AcmeApiTest.kt
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2019 Markus Ressel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+ * to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package de.markusressel.freenasrestapiclient.api.v2
+
+import com.github.salomonbrys.kotson.jsonObject
+import de.markusressel.freenasrestapiclient.api.v2.base.TestBase
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class AcmeApiTest : TestBase() {
+
+    @Test
+    fun testGetAuthenticatorSchemas() {
+        runBlocking {
+            val result = underTest.getAuthenticatorSchemas()
+            result.fold(success = {
+                println("$it")
+            }, failure = {
+                throw it
+            })
+        }
+    }
+
+    @Test
+    fun testCreateAcmeConfig() {
+        runBlocking {
+            val result = underTest.createAcmeConfig("test", "test", jsonObject())
+            result.fold(success = {
+                println("$it")
+            }, failure = {
+                throw it
+            })
+        }
+    }
+
+    @Test
+    fun testDeleteAcmeConfig() {
+        runBlocking {
+            val result = underTest.deleteAcmeConfig(0)
+            result.fold(success = {
+                println("$it")
+            }, failure = {
+                throw it
+            })
+        }
+    }
+
+    @Test
+    fun testGetAcmeDnsAuthenticators() {
+        runBlocking {
+            val result = underTest.getAcmeDnsAuthenticators()
+            result.fold(success = {
+                println("$it")
+            }, failure = {
+                throw it
+            })
+        }
+    }
+
+    @Test
+    fun testUpdateAcmeDnsAuthenticators() {
+        runBlocking {
+            val result = underTest.updateAcmeDnsAuthenticators(
+                    0,
+                    "test",
+                    jsonObject()
+            )
+            result.fold(success = {
+                println("$it")
+            }, failure = {
+                throw it
+            })
+        }
+    }
+
+}
